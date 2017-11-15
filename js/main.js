a44c4f5e8e27edd2506b25b645dc6f9102685248
stain88 <marcbaghdadi@gmail.com>
2017-11-15 16:29:53 +0000

    Ball is bouncing around canvas

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
+// canvas settings
 var canvas;
 var CANVAS_HEIGHT = 640;
 var CANVAS_WIDTH = 800;
-var playerY;
+
+// paddle settings
+var player_Y;
 var PADDLE_HEIGHT = 80;
 var PADDLE_WIDTH = 20;
+
+// ball settings
+var ball_X;
+var ball_Y;
+var ball_direction_X;
+var ball_direction_Y;
+var ball_speed_X;
+var ball_speed_Y;
+var BALL_RADIUS = 20;
 
 function setup() {
   canvas = createCanvas(CANVAS_WIDTH,CANVAS_HEIGHT);
   canvas.parent('game-container');
   background('#2dd5e3');
 
-  playerY = 240;
+  player_Y = (CANVAS_HEIGHT / 2) - (PADDLE_HEIGHT / 2);
+  ball_X = CANVAS_WIDTH / 2;
+  ball_Y = CANVAS_HEIGHT / 2;
+  ball_direction_X = (Math.random() < 0.7) ? -1 : 1;
+  ball_direction_Y = (Math.random() < 0.5) ? -1 : 1;
+  ball_speed_X = ball_direction_X * Math.floor(Math.random() * 6 + 3);
+  ball_speed_Y = ball_direction_Y * Math.floor(Math.random() * 4 + 2);
 }
 
 function draw() {
@@ -18,6 +36,7 @@ function draw() {
   draw_background();
   check_for_movement();
   draw_player_paddle();
+  move_ball();
 }
 
 function draw_background() {
@@ -26,14 +45,26 @@ function draw_background() {
 
 function check_for_movement() {
   if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
-    playerY -= 5;
-    playerY = max(playerY, 0);
+    player_Y -= 5;
+    player_Y = max(player_Y, 0);
   } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
-    playerY += 5;
-    playerY = min(playerY, CANVAS_HEIGHT - PADDLE_HEIGHT);
+    player_Y += 5;
+    player_Y = min(player_Y, CANVAS_HEIGHT - PADDLE_HEIGHT);
   }
 }
 
 function draw_player_paddle() {
-  rect(20, playerY, PADDLE_WIDTH, PADDLE_HEIGHT);
+  rect(20, player_Y, PADDLE_WIDTH, PADDLE_HEIGHT);
+}
+
+function move_ball() {
+  ball_X += ball_speed_X;
+  if (ball_X - BALL_RADIUS < 0 || ball_X + BALL_RADIUS > CANVAS_WIDTH) {
+    ball_speed_X *= -1;
+  }
+  ball_Y += ball_speed_Y;
+  if (ball_Y - BALL_RADIUS < 0 || ball_Y + BALL_RADIUS > CANVAS_HEIGHT) {
+    ball_speed_Y *= -1;
+  }
+  ellipse(ball_X, ball_Y, BALL_RADIUS, BALL_RADIUS);
 }
